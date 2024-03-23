c7dc989dd16d792a83a85dede4e493a95c56b3cb
Jose Bobadilla <joseboba18@gmail.com>
2024-03-23 08:44:03 -0700

    FIX COMPONENT

--- src/modules/administration/routes/routesAdministration.ts
+++ src/modules/administration/routes/routesAdministration.ts
@@ -1,10 +1,12 @@
 import {SidNavItemInterface} from "../../../interfaces";
 import {ADMIN_BASE_PATH} from "../../../util";
+import {DepartmentPage} from "../pages";
 
 
 export const routesAdministration: SidNavItemInterface[] = [
     {
         path: 'department',
-        to: `${ADMIN_BASE_PATH}/department`
+        to: `${ADMIN_BASE_PATH}/department`,
+        Component: DepartmentPage
     }
 ];
